d2025be34f5c5e1256d5725cecafcda09cb3b975
Sarang Shinde <sarang.shinde@clix.capital>
2022-09-12 23:32:40 +0530

    initial comit

---
+++ src/main/java/com/sarangetch/cicd/controller/Test.java
@@ -0,0 +1,16 @@
+package com.sarangetch.cicd.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    @GetMapping(value = "/get/{name}")
+    public ResponseEntity<?> getName(@PathVariable("name") String name) {
+        return new ResponseEntity<>("hi  " + name, HttpStatus.OK);
+
+    }
+}
